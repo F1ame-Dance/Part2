b8b27b8500c61c66dce49ccf138f715be20949ae
FlameDance <FlameDance@DESKTOP-16QNFGI>
2021-04-13 20:36:45 +0300

    Start Part2

---
+++ WebStore/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+using Microsoft.AspNetCore.Mvc;
+
+using WebStore.Models;
+
+namespace WebStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View();
+    }
+}
