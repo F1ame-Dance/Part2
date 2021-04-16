212014d44cf2da9cf7bf8801103fab7b6a228e89
FlameDance <FlameDance@DESKTOP-16QNFGI>
2021-04-16 13:18:11 +0300

    WebApi

--- Services/WebStore.Interfaces/TestAPI/IValuesService.cs
+++ Services/WebStore.Interfaces/TestAPI/IValuesService.cs
@@ -1,20 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace WebStore.Interfaces.TestAPI
 {
-    public interface IValueService
+    public interface IValuesService
     {
         IEnumerable<string> Get();
 
         string Get(int id);
-        Uri Create(string value);
-        HttpStatusCode Edit(int id, string value);
-        HttpStatusCode Remove(int id);
 
+        Uri Create(string value);
+
+        HttpStatusCode Edit(int id, string value);
+
+        bool Remove(int id);
     }
 }
